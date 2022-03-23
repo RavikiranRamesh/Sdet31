b366ad93a61119d978eab27bfc52feecb3305803
ADMIN <ADMIN@RavikiranDESKTOP-GLPREHU>
2022-03-23 17:36:21 +0530

    class-added

---
+++ com.crm.vitiger/src/test/java/com/crm/vitigersample/SamplecheckTest.java
@@ -0,0 +1,11 @@
+package com.crm.vitigersample;
+
+import org.testng.annotations.Test;
+
+public class SamplecheckTest {
+	@Test
+	public void check() {
+		System.out.println("Check in github");
+	}
+
+}
