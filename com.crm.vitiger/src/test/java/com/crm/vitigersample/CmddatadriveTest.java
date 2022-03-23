97ff68f694c602d0ba3ec1ab0e7f3fe7e0fd33a0
ADMIN <ADMIN@RavikiranDESKTOP-GLPREHU>
2022-03-23 17:17:41 +0530

    Demo-project

---
+++ com.crm.vitiger/src/test/java/com/crm/vitigersample/CmddatadriveTest.java
@@ -0,0 +1,46 @@
+package com.crm.vitigersample;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CmddatadriveTest {
+	WebDriver driver=null;
+	@Test
+	public void CmdData() {
+		String browser= System.getProperty("BROWSER");
+		String url= System.getProperty("URL");
+		String password= System.getProperty("PASSWORD");
+		String username= System.getProperty("USERNAME");
+			if(browser.equalsIgnoreCase("chrome")) {
+				WebDriverManager.chromedriver().setup();
+				driver=new ChromeDriver();
+				
+			}else if(browser.equalsIgnoreCase("firefox")) {
+				WebDriverManager.firefoxdriver().setup();
+				driver=new FirefoxDriver();
+			}else {
+				System.out.println("assign the browser");
+			}
+			
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.get(url);
+			driver.findElement(By.name("user_name")).sendKeys(username);
+			driver.findElement(By.name("user_password")).sendKeys(password);
+			driver.findElement(By.id("submitButton")).click();
+			System.out.println(browser);
+			System.out.println(url);
+			System.out.println(username);
+			System.out.println(password);
+		driver.quit();
+		
+	}
+
+}
---
+++ com.crm.vitiger/src/test/java/com/crm/vitigersample/CreatorgsampleTest.java
@@ -0,0 +1,15 @@
+package com.crm.vitigersample;
+
+import org.testng.annotations.Test;
+
+public class CreatorgsampleTest {
+	@Test
+	public void Creatorg() {
+			System.out.println(" Organization1 created successfully ");
+	}
+    @Test
+    public void Createorg2() {
+    	System.out.println(" Organization2 created successfully ");
+    }
+
+}
